e28f9f1c95a34b7e853d640184c49b8e5476ae73
AnkeVanBeek-IDgis <anke.van.beek@idgis.nl>
2024-06-03 14:24:14 +0200

    add comment and add more example info

--- db/sql/001-setup.sql
+++ db/sql/001-setup.sql
@@ -9,15 +9,22 @@ create table api.todos (
   due timestamptz
 );
 
-insert into api.todos (task) values
-  ('finish tutorial 0'), ('pat self on back');
- 
+insert into api.todos (task, done) values
+  ('Beginnen bij het begin', 'true'),
+  ('doorgaan met de volgende stap', 'false'),
+  ('blijven volhouden', 'false'),
+  ('stoppen bij het einde', 'false');
+
+
+-- maak een role voor de anonieme web-requests.
+-- Als er een request gemaakt wordt, dan schakelt PostgREST naar deze rol over om de queries uit te voeren.
+-- geef alle rechten (dus all in plaats van select)
 create role web_anon nologin;
 
 grant usage on schema api to web_anon;
--- geef alle rechten (dus all in plaats van select)
 grant all on api.todos to web_anon;
 grant all on sequence api.todos_id_seq to web_anon;
 
+-- daarnaast maken we een tweede rol aan, die rechten kan overnemen van de web_anon-rol
 create role authenticator noinherit login password 'postgres';
 grant web_anon to authenticator;
